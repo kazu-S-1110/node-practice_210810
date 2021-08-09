ba57477e3db60c00196f439aa39cc8ae01532662
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-10 08:16:17 +0900

    Json形式で書き出す方法を学習。知ってたけど改めて認識した。

--- helper.js
+++ helper.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
 const name = 'mike';
 const add = (a, b) => a + b;
+
+const person = {
+  name: 'hoge',
+  age: 20,
+};
+
 const read = () => {
-  fs.readFile('./sample.txt', 'utf8', (err, data) => {
+  fs.readFile('./sample.json', 'utf8', (err, data) => {
     console.log(data);
   });
 };
 const write = () => {
-  fs.writeFile('./sample.txt', 'hello world', () => {
+  // personだけだとエラーになる。json形式で渡すのではなくテキストにして渡す必要あり(JSON.stringifyを使う)。
+  fs.writeFile('./sample.json', JSON.stringify(person), () => {
     console.log('write file completed!');
   });
 };
