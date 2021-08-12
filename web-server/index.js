332f91d8dfa351c4800e7bf9bcee131797a2d7ad
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-12 12:57:25 +0900

    sendの返し方を学習。

--- web-server/index.js
+++ web-server/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello World, I comin' new World!");
+  res.send("<h1>Hello World, I comin' new World!</h1>"); //htmlを返すことが可能
+});
+
+app.get('/api/v1/user', (req, res) => {
+  res.send({
+    //jsonを返すことも可能
+    name: 'Mike',
+    age: 39,
+  });
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port);
-console.log(`listening ${port} port`);
+app.listen(port, () => console.log(`listening ${port} port`));
