f024ed36143d49304ea1dbc42b3143c69456ed8a
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-14 20:51:40 +0900

    formからのpostを取得する方法を学習。

--- web-server/index.js
+++ web-server/index.js
@@ -3,11 +3,17 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public'))); //public内のページを読み込んでくれる。
-
+app.use(express.urlencoded({ extended: false })); //これでreqのbodyを取得できる。
 // app.get('/', (req, res) => {
 //   console.log(req); //reqを見ることが可能、めっちゃ情報が多い！
 //   res.send(); //htmlを返すことが可能
 // });
+
+app.post('/api/v1/quiz', (req, res) => {
+  const ans = req.body.answer; //formのnameから取得が可能
+  console.log(ans);
+  res.send(ans);
+});
 
 app.get('/api/v1/user', (req, res) => {
   res.send({
